c03d8ff222c9e6332bffbfe3dc858cbe993fd3d4
DominikSubocz <dominiksubocz@gmail.com>
2023-10-26 11:39:09 +0100

    Added a function to js file
    Added so if the windows is wider, the bars are shorter and other way round

--- scripts/main.js
+++ scripts/main.js
@@ -22,6 +22,9 @@ fetch(jsonFilePath)
   .then(data => {
     console.log(data); // Display the data in the console as an example
     updateDivHeights(data);
+    window.onresize = function(event) {
+      updateDivHeights(data);
+    }
   })
   .catch(error => {
     console.error('Error fetching JSON data:', error); // Display error.
@@ -30,19 +33,37 @@ fetch(jsonFilePath)
 
   // =======================================================================================================================================================
 
-
 // Define a function to update the div heights
 function updateDivHeights(data) {
   const maxHeight = Math.max(...data.map(item => item.amount));
   const divs = document.querySelectorAll(".chart-bar"); // Replace with your div class or selector
 
+// For each loop, that goes through each div
   divs.forEach((item, index) => {
     const amount = data[index].amount;
-    const height = (amount / maxHeight) * 35 + "vw"; // Assuming your CSS uses percentage-based heights
+    height = 0;
 
-    divs[index].style.height = height;
+   
+    // If windows is wider, the bars are shorter!
+    if(window.innerWidth >= 600){
+    height = (amount / maxHeight) * 25 + "vw"; 
+    }
+
+    // If window is narrower, the bars are longer!
+    else{ 
+    height = (amount / maxHeight) * 35 + "vw"; 
+    }
+    
+    divs[index].style.height = height; // Basically saying that the height of this div at this position should be equal to the height variable.
+    
+
+    // For each loop that goes thru each div again
     divs.forEach((div) => {
-      const dayFromData = div.getAttribute("data-day");
+      const dayFromData = div.getAttribute("data-day"); // Getting the date
+     
+     // This if looks if the div's name which is assigned from the JSON file, and checks if it's equal to today.
+     // If it is equal to today, it will add a class name 'today' to the specific div.
+     // If not it will do the same, but instead it will be 'other-day'.
       if (dayFromData === currentDay) {
         div.classList.add('today'); // Add a CSS class for today's div
       } else {
